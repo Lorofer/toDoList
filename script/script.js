5e63691171c967f1bd0619020c83d965774671cb
Lorofer <lev.buben@gmail.com>
2024-02-03 23:42:14 +0300

    Приложение было переписано. Теперь используется Vue3. Добавлена возможность удаления добавленных задач

--- script/script.js
+++ script/script.js
@@ -1,38 +1,18 @@
-function displayForm() {
-    addTaskButton.style.display = "none";
-
-    let createTaskForm = `
-    <form name="createTaskForm">
-        <input name="taskText" type="text">
-        <button type="button" id="createTaskButton">Добавить задачу</button>
-    </form>
-    `;
-
-    let main = document.getElementById('main');
-    main.insertAdjacentHTML("beforeend", createTaskForm);
-
-    let createTaskButton = document.getElementById('createTaskButton');
-    createTaskButton.onclick = function () {
-        createTask();
-        removeForm();
-    };
-}
-
-function createTask() {
-    let taskText = document.createTaskForm.taskText.value;
-
-    let task = `
-    <p>${taskText}</p>
-    `;
-
-    let list = document.getElementById('tasks');
-    list.insertAdjacentHTML("beforeend", task);
-}
-
-function removeForm() {
-    document.forms.createTaskForm.remove();
-    addTaskButton.style.display = "block";
-}
-
-let addTaskButton = document.getElementById('addTaskButton');
-addTaskButton.onclick = displayForm;
+Vue.createApp({
+    data(){
+        return {
+            showTaskForm: false,
+            newTaskText: '',
+            taskList: [],
+        };
+    },
+    methods: {
+        createTask(text) {
+            if (text != '') {
+                this.taskList.push(text);
+            }
+            this.showTaskForm = !this.showTaskForm;
+            console.log(this.taskList);
+        }
+    },
+}).mount('#main');
